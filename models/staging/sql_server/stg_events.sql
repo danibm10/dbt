894e7938837573f6518f7a065b5c7ebe388b972f
dani20694@gmail.com <dani20694@gmail.com>
2022-12-01 18:39:03 +0000

    adiooooos

--- models/staging/sql_server/stg_events.sql
+++ models/staging/sql_server/stg_events.sql
@@ -20,8 +20,8 @@ events_v1 AS (
         , CAST(SUBSTRING(created_at, 1, 10) AS DATE) AS created_at_day
         , CAST(SUBSTRING(created_at, 12, 8) AS TIME) AS created_at_time
         , order_id
-        , CAST(SUBSTRING(_fivetran_synced, 1, 10) AS DATE) AS FECHA_SINCRONIZACION
-        , CAST(SUBSTRING(_fivetran_synced, 12, 8) AS TIME) AS HORA_SINCRONIZACION
+        , CAST(SUBSTRING(_fivetran_synced, 1, 10) AS DATE) AS sync_date
+        , CAST(SUBSTRING(_fivetran_synced, 12, 8) AS TIME) AS sync_time
     FROM src_sql_server_events
     )
 
