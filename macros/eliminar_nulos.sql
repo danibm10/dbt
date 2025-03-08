fa37ebbb0489818bc69961fd97f667a6e1235316
dani20694@gmail.com <dani20694@gmail.com>
2025-03-08 17:40:07 +0000

    eliminación todo

--- macros/eliminar_nulos.sql
+++
@@ -1,8 +0,0 @@
-{% macro eliminar_nulos(columna,actual,cambio="'Unknown'") %}
-
-case
-    when ifnull({{ columna }},'pepon')=ifnull({{ actual }},'pepon') then {{ cambio }}
-
-    else {{ columna }}
-end
-{% endmacro %}
